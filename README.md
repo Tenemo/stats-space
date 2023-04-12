89ac6ae6b6878b8ce3bbcef5198cef0707ee535f
Piotr Piech <piotr.piech@nearform.com>
2023-04-12 22:52:34 +0200

    updated sentry dsn

--- README.md
+++ README.md
@@ -8,3 +8,5 @@
 
 -   deployment diagrams
 -   docker
+-   favicon
+-   sentry
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import * as Sentry from '@sentry/react';
-import { BrowserTracing } from '@sentry/tracing';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import Root from 'components/Root';
 
 Sentry.init({
-    dsn: 'https://ae9f630bfc884a3a86d5a3bbb5ae29c4@o502294.ingest.sentry.io/4504912262463488',
-    integrations: [new BrowserTracing()],
+    dsn: 'https://dce057c40dec416d90f4a5e57c4cbd78@o502294.ingest.sentry.io/4504997651218432',
+    integrations: [new Sentry.BrowserTracing()],
     tracesSampleRate: 1.0,
 });
 
