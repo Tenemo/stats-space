8302e16ef9d7273737a1cf8525a99676180af224
Piotr Piech <piotr.piech@nearform.com>
2023-04-13 20:59:43 +0200

    cleaned up Root

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -1,14 +1,11 @@
-import { CircularProgress, Box } from '@mui/material';
+import { CircularProgress } from '@mui/material';
 import React, { ReactElement, useEffect } from 'react';
 import { HelmetProvider } from 'react-helmet-async';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
-import packageJson from '../../../package.json';
-
 import App from 'components/App';
 import { store, persistor } from 'store';
-import { getAppStoreVersion } from 'store/app/appSelectors';
 
 import 'styles/global.scss';
 
@@ -24,33 +21,6 @@ export const Root = (): ReactElement => {
             }
         });
     }, []);
-    const currentStoreVersion = getAppStoreVersion(store.getState());
-
-    useEffect(() => {
-        if (
-            !!currentStoreVersion &&
-            currentStoreVersion !== packageJson.version
-        ) {
-            void persistor.purge();
-            store.dispatch({ type: 'RESET_STATE' });
-        }
-    }, [currentStoreVersion]);
-    if (!!currentStoreVersion && currentStoreVersion !== packageJson.version) {
-        return (
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    height: '100%',
-                    width: '100%',
-                }}
-            >
-                <CircularProgress />
-            </Box>
-        );
-    }
     return (
         <Provider store={store}>
             <PersistGate loading={<CircularProgress />} persistor={persistor}>
