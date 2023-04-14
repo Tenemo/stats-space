bbb6cfc781d09ec018e51c1f84b16079644ccd0b
Piotr Piech <piotr.piech@nearform.com>
2023-04-15 00:35:35 +0200

    font adjustments

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -15,7 +15,7 @@ const common = {
                     font-style: normal;
                     font-display: swap;
                     font-weight: 400;
-                    src: local('Roboto Mono'), local('Roboto Mono-Regular'), url(${
+                    src: local('Roboto Mono'), url(${
                         RobotoMonoWoff2 as string
                     }) format('woff2');
                     unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
